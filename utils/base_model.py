9f7ff2c8e33d639eb079009911eda9b41f2a76a5
Kushagra Yadav <kushagrayadv15@gmail.com>
2024-11-29 15:36:03 -0500

    layer fixes

--- utils/base_model.py
+++ utils/base_model.py
@@ -22,25 +22,11 @@ class BaseModel(nn.Module, ABC):
         nn.Linear(768, 3)       # Embedding size is 768
     )
 
-    self.cls_emb = nn.Embedding(num_embeddings=1, embedding_dim=768)            # class embedding layer
-
     # Attention layers
     bert_config = BertConfig(num_hidden_layers=config.num_attention_layers)
     self.attention_encoder_layers = nn.ModuleList(
         [AttentionEncoder(bert_config) for _ in range(config.num_attention_layers)]
     )
-
-  def prepend_class(self, inputs: Tensor, masks: Tensor) -> Tuple[Tensor, Tensor]:
-      index = torch.LongTensor([0]).to(device=inputs.device)
-      cls_emb = self.cls_emb(index)
-      cls_emb = cls_emb.expand(inputs.size(0), 1, inputs.size(2))
-      outputs = torch.cat((cls_emb, inputs), dim=1)
-
-      cls_mask = torch.ones(inputs.size(0), 1, inputs.size(2)).to(device=inputs.device)
-      print("shapes", cls_mask.shape, masks.shape)
-      masks = torch.cat([cls_mask, masks], dim=1)
-
-      return outputs, masks
 
 
   @abstractmethod
--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -20,9 +20,8 @@ class MVSADataLoaders:
         train_dataset = ImageDataset(train_paths, train_labels)
         test_dataset = ImageDataset(test_paths, test_labels)
 
-        train_loader = DataLoader(train_dataset, batch_size=4, shuffle=False)
-        train_loader = DataLoader(train_dataset, batch_size=4, shuffle=False)
-        test_loader = DataLoader(test_dataset, batch_size=2, shuffle=False)
+        train_loader = DataLoader(train_dataset, batch_size=32, shuffle=False)
+        test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
         return train_loader, test_loader
 
@@ -30,25 +29,18 @@ class MVSADataLoaders:
         train_size = int(0.8 * len(labels))  # 80% train, 20% test
 
         train_data = text[:train_size]
-        train_data = text[:train_size]
         train_labels = labels[:train_size]
 
         test_data = text[train_size:]
-        test_data = text[train_size:]
         test_labels = labels[train_size:]
 
-        train_dataset = TextTokenizer(train_data,train_labels).get_dataset()
-        test_dataset = TextTokenizer(test_data,test_labels).get_dataset()
+        train_dataset = TextTokenizer(train_data, train_labels).get_dataset()
+        test_dataset = TextTokenizer(test_data, test_labels).get_dataset()
 
         train_dataset = TextDataset(train_dataset, train_labels)
         test_dataset = TextDataset(test_dataset, test_labels)
 
-        train_loader = DataLoader(train_dataset, batch_size=4, shuffle=False)
-        train_dataset = TextTokenizer(train_data,train_labels).get_dataset()
-        test_dataset = TextTokenizer(test_data,test_labels).get_dataset()
-        train_dataset = TextDataset(train_dataset, train_labels)
-        test_dataset = TextDataset(test_dataset, test_labels)
-        train_loader = DataLoader(train_dataset, batch_size=4, shuffle=False)
-        test_loader = DataLoader(test_dataset, batch_size=2, shuffle=False)
+        train_loader = DataLoader(train_dataset, batch_size=32, shuffle=False)
+        test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
         return train_loader, test_loader
